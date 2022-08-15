e792d0709cbd6284a4158c1e09406514564ecb03
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-08-15 15:42:42 +0200

    Render GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import { Platform, KeyboardAvoidingView } from 'react-native';
 
 export default class Chat extends React.Component {
   constructor(props) {
@@ -8,6 +9,7 @@ export default class Chat extends React.Component {
     this.state = {
       name: '',
       bgColor: '#090C08',
+      messages: [],
     };
   }
   componentDidMount() {
@@ -17,10 +19,37 @@ export default class Chat extends React.Component {
     //Set Background color accroding to the state through props
     const { bgColor } = this.props.route.params;
     this.setState({ bgColor });
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello Developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    });
+  }
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
   }
   render() {
     return (
       <View style={[styles.container, { backgroundColor: this.state.bgColor }]}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
         <Button title="Go to Start" onPress={() => this.props.navigation.navigate('Start')} />
       </View>
     );
@@ -30,7 +59,5 @@ export default class Chat extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
